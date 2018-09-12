007d4091d8c3805623f91cc00356913611859fa1
MuhammadTausif <tausifasia@yahoo.com>
2018-09-12 06:30:52 +0500

    Initial app

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+  posts$: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.posts$ = data
+    );
+  }
+
+}
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-posts',
+//   templateUrl: './posts.component.html',
+//   styleUrls: ['./posts.component.scss']
+// })
+// export class PostsComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
